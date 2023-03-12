b41f149d6b11b26368eb17dab4d7d02bc7bfb5cb
福島貴裕 <fukushimatakahiro@tk-mac.local>
2023-03-12 13:19:47 +0900

    mysql変更後立ち上げ確認まで

--- docker-compose/mysql/init/init.sql
+++ docker-compose/mysql/init/init.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `geoglyph_composer`;
-GRANT ALL ON geoglyph_composer.* TO 'root'@'%';
+CREATE USER 'geoglyph' IDENTIFIED BY 'ymH87uKm6ECN22YxegJHV';
+GRANT ALL PRIVILEGES ON geoglyph_composer.* TO 'geoglyph';
